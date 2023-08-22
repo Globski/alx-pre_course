e5f8f1b2fde598b3f990a1e4fe8810f789ae2bd6
Globski <gloria.ogunsemore@gmail.com>
2023-08-22 10:23:52 -0700

    A simple Hello world! program

---
+++ Gloria-C-Project/hello-world-program.c
@@ -0,0 +1,14 @@
+// A simple Hello world! program
+// workig with functions 
+
+#include<stdio.h>
+ void func_name(){
+        printf("Hello world!");
+    }
+
+int main() {
+   func_name();
+ 
+    
+return 0;
+}
