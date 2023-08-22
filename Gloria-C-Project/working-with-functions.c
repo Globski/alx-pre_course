9dc3d14b3132ea2f29e8a63a75c43aec24d133bf
Globski <gloria.ogunsemore@gmail.com>
2023-08-22 10:13:27 -0700

    my first go on working with functions

---
+++ Gloria-C-Project/working-with-functions.c
@@ -0,0 +1,14 @@
+// my first go on c language
+// workig with functions 
+
+#include<stdio.h>
+ void func_name(char school_files [], int grade){
+        printf("%s your grade is: %d", school_files, grade);
+    }
+
+int main() {
+   func_name("Mike", 75);
+ 
+    
+return 0;
+}
